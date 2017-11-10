521a3b036a596cec12d75f93458be72d4b434d8d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-11-10 18:41:02 +0800

    updated main.sh to check if failed to read from file; updated stop-all.sh in case running in with debug flag (-x);

--- main.sh
+++ main.sh
@@ -2,17 +2,30 @@
 set -e
 input=$1
 while true; do
-  output=$(echo "$input" | sha256sum | awk '{print $1}')
+  output=""
+  while [ "$output" == "" ]; do
+    ## maybe opening too many files
+    output=$(echo "$input" | sha256sum | awk '{print $1}')
+  done
   #echo "$input ~~> $output"
+  if [ "$output" == "" ]; then
+    echo "error: hash is empty"
+    exit 1
+  fi
   if [ -f $output ]; then
-    old=$(cat $output)
+    old=""
+    while [ "$old" == "" ] && [ "$output" != "01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b" ]; do
+      ## in case opening too many files
+      old=$(cat $output)
+    done
     if [ "$old" != "$input" ]; then
-      echo "collision found, both hash to $output"
-      echo "$old"
-      echo "$input"
+      echo "collision found, both hash to $output" | tee -a log
+      echo "$output <--1: $old" | tee -a log
+      echo "$output <--2: $input" | tee -a log
+      bash stop-all.sh
       exit 0
-    else
-      echo "skip $output"
+    #else
+    #  echo "skip $output"
     fi
   else
     echo "$input" > "$output"
--- stop-all.sh
+++ stop-all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps aux | grep 'bash main.sh' | grep -v grep | awk '{print $2}' | xargs -I {} kill {}
+ps aux | grep 'main.sh' | grep -v grep | awk '{print $2}' | xargs -I {} kill {} 2>&1 | grep -v 'No such process' | grep -v 'terminated'
