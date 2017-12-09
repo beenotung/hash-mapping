650e29831ebe86340bab1ed634cbf4d6aecbdac2
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-12-10 02:29:46 +0800

    updated clean script to speed up the process

--- clean.sh
+++ clean.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-ls | grep sh -v | xargs rm
+tmp="../.hash-mapping/"
+mkdir -p "$tmp"
+git ls-files | xargs -I {} mv {} "$tmp"
+mv .git "$tmp"
+cd ..
+mv hash-mapping hash-mapping.del
+mv .hash-mapping hash-mapping
+rm -rf hash-mapping.del
